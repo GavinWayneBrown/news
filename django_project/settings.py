7a7942451197f64ec1d06ea98b58e48efbe2cfac
Gavin <gavinwaynebrown@gmail.com>
2024-10-28 21:34:05 -0600

    newspaper app

--- django_project/settings.py
+++ django_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "crispy_bootstrap5",
     "accounts",
     "pages",
+    "articles",
 
 
 ]
@@ -138,3 +139,4 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"  # new
 CRISPY_TEMPLATE_PACK = "bootstrap5"  # new
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+TIME_ZONE = "America/New_York"
